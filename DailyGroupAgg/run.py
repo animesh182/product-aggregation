2300c58942eafb2053378ff0bb79a353a1a356c1
Animesh Adhikari <animeshadhikari38@gmail.com>
2024-04-05 12:19:22 +0545

    Small fixes

--- DailyGroupAgg/run.py
+++ DailyGroupAgg/run.py
@@ -4,15 +4,16 @@ from datetime import datetime, timedelta
 from DailyGroupAgg.fetch_agg_group_data import extract_data
 from DailyGroupAgg.import_daily_group_agg import load_data
 
+
 def main(myTimer: func.TimerRequest) -> None:
     today = datetime.today()
     first_day_this_month = today.replace(day=1)
     start_date = (first_day_this_month - timedelta(days=1)).replace(day=1)
-    end_date = first_day_this_month - timedelta(days=1)  
-
+    end_date = first_day_this_month - timedelta(days=1)
+    logging.info(first_day_this_month)
+    logging.info(end_date)
     # Extract
     agg_data = extract_data(start_date, end_date)
-
     # Load
     load_data(agg_data)
 
--- params.py
+++ params.py
@@ -1,16 +1,16 @@
 # production
-# params = {
-#     "dbname": "salesdb1",
-#     "user": "salespredictionsql",
-#     "password": "Shajir86@ms9",
-#     "host": "sales-prediction-svr-v2.postgres.database.azure.com",
-#     "port": "5432",
-# }
-# staging
 params = {
-    'dbname': 'salesdb1',
-    'user': 'salespredstaging',
-    'password': 'Shajir86@ms9',
-    'host': 'krunch-staging-svr.postgres.database.azure.com',
-    'port': '5432'
+    "dbname": "salesdb1",
+    "user": "salespredictionsql",
+    "password": "Shajir86@ms9",
+    "host": "sales-prediction-svr-v2.postgres.database.azure.com",
+    "port": "5432",
 }
+# staging
+# params = {
+#     'dbname': 'salesdb1',
+#     'user': 'salespredstaging',
+#     'password': 'Shajir86@ms9',
+#     'host': 'krunch-staging-svr.postgres.database.azure.com',
+#     'port': '5432'
+# }
