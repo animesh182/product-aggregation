069b4564ca1a9d98b6e9a0cd7e8f13920569117a
Animesh Adhikari <animeshadhikari38@gmail.com>
2024-06-03 11:27:57 +0545

    Small fixes

--- DailyGroupAgg/run.py
+++ DailyGroupAgg/run.py
@@ -8,12 +8,12 @@ from DailyGroupAgg.import_daily_group_agg import load_data
 def main(myTimer: func.TimerRequest) -> None:
     today = datetime.today()
     # first day of current month
-    start_date = today.replace(day=1)
+    start_date = today.replace(day=1).date()
     # first day of next month
     first_day_next_month = (start_date + timedelta(days=32)).replace(day=1)
+
     # Then, subtract one day from the first day of next month to get the last day of the current month
     end_date = first_day_next_month - timedelta(days=1)
-
     # Extract
     agg_data = extract_data(start_date, end_date)
     # Load
